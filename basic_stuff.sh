b83d166ecf8ceac602f0eebb67ab2aa6e19f8cef
ricardo-f <53285903+ricardo-f@users.noreply.github.com>
2020-10-17 16:17:50 -0300

    auto confirmation added and removed x11-util

--- basic_stuff.sh
+++ basic_stuff.sh
@@ -1,18 +1,19 @@
 apt update
 apt upgrade -y
-apt install xorg openbox slim
-apt install suckless-tools
-apt install feh
-apt install conky-all
-apt install x11-utils
-apt install mesa-utils
-apt install xbindkeys
-apt install terminator
-apt install tree
-apt install pcmanfm
-apt install iceweasel
-apt install alpine
+apt install xorg openbox slim -y
+apt install suckless-tools -y
+apt install feh -y
+apt install conky-all -y
+apt install mesa-utils -y
+apt install xbindkeys -y
+apt install terminator -y
+apt install tree -y
+apt install pcmanfm -y
+apt install iceweasel -y
+apt install alpine -y
+apt install curl -y
+apt install unzip -y
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 apt update
-apt install spotify-client
+apt install spotify-client -y
