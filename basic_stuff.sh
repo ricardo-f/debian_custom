d49ffa83d45b95efac607ebd806fd3e9695f6b84
ricardo-f <53285903+ricardo-f@users.noreply.github.com>
2021-02-18 22:46:43 -0300

    Removing Kite

--- basic_stuff.sh
+++ basic_stuff.sh
@@ -21,7 +21,6 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 apt-get update
 apt-get install atom -y
-bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)"
 apt-get install software-properties-common -y
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
